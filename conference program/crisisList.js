3b7f64c1ab96a0ab136191986ea9cc8d34bea71f
rcheng11 <48870179+rcheng11@users.noreply.github.com>
2021-04-10 10:51:37 -0400

    Update crisisList.js

--- conference program/crisisList.js
+++ conference program/crisisList.js
@@ -14,22 +14,6 @@ function addEventListenerCrisisClose(){
     $(this).parent().remove()
   })
 }
-// Committees
-let committeeCountryListsCrisis = {
-  "NATO": ['United States', 'Turkey', 'Greece', 'Ukraine', 'UK', 'France', 'Poland', 'Germany', 'Estonia', 'Latvia', 'Lithuania', 'North Macedonia', 'Bulgaria', 'Hungary', 'Romania', 'Spain', 'Italy', 'Canada', 'Denmark', 'Belgium', 'Netherlands', 'Portugal', 'Albania', 'Montenegro', 'Czech', 'Republic', 'Slovakia', 'Slovenia', 'Norway'],
-  "UNCTAD":  ['Canada', 'Russia', 'United States', 'Venezuela', 'Egypt', 'China', 'UK', 'Bolivia', 'Norway', 'Saudi Arabia', 'DPRK', 'Panama', 'Nigeria', 'India', 'France', 'Denmark', 'Angola', 'Chile', 'Malaysia', 'Indonesia', 'Afghanistan', 'Australia', 'New Zealand', 'Brazil', 'Uganda', 'South Korea', 'Japan', 'Botswana', 'UAE'],
-  "UNHRC":['Yemen', 'USA', 'Saudi Arabia', 'Syria', 'UK', 'Brazil', 'Venezuela', 'India', 'Nigeria', 'Philippines', 'France', 'Iran', 'Belgium', 'Italy', 'Sweden', 'Switzerland', 'Denmark', 'Germany', 'Afghanistan', 'Turkey', 'Pakistan', 'Ghana', 'Egypt', 'Bangladesh', 'Kenya', 'Japan', 'China', 'DPRK (North Korea)', 'South Korea'],
-  "WHO":["India","Italy","South Korea","France","Vietnam","Australia","United Kingdom","Israel","Egypt","Czech Republic","Japan","China","United States","Ethiopia","Iran","Brazil"],
-  "ECOSOC":["Argentina","Armenia","Australia","Bolivia","Botswana","Brazil","Canada","China","Colombia","DPRK","Egypt","Ethiopia","France","Germany","Iran (Islamic Republic Of)","Japan","Kenya","Mexico","Nigeria","Pakistan","Panama","Portugal","Republic of Korea","Russian Federation","Saudi Arabia","Switzerland","Urkraine","UK","USA","Zimbabwe"],
-  "US Space Force":['Chief of Space Operations', 'Vice Chief of Space Operations', 'Chief Master Sergeant of the Space Force', 'Vice President of the United States', 'White House Chief of Staff', 'Director of the CIA', 'Director of Staff', 'Chief Human Capital Officer', 'Chief Operations Officer', 'Director of Intelligence, Surveillance, and Reconnaissance', 'U.S. Ambassador to the United Nations', 'United States Secretary of the Treasury', 'Administrator of NASA', 'Commander of the Space Operations Command: Peterson Air Force Base', 'Commander of the Space Operations Command West: Vandenberg Air Force Base', 'Chief Strategy and Resourcing Officer', 'Director of Plans and Programs', 'Chief Technology and Innovation Officer'],
-  "UNEP":['US', 'Britain', 'China', 'Canada', 'France', 'Russia', 'Germany', 'Australia', 'North Korea', 'Pakistan', 'New Zealand', 'Iraq', 'Japan', 'Brazil', 'Italy', 'Israel', 'Sweden', 'Switzerland', 'Ukraine', 'Turkey', 'Mexico', 'Spain', 'Yemen', 'Portugal', 'Chile', 'Ireland', 'Argentina', 'Egypt', 'Greece'],
-  "Arab League":["Algeriia","Bahrain","Comoros","Dijbouti","Egypt","Iraq","Jordan","Kuwait","Lebanon","Mauritania","Morocco","Oman","Qatar","Saudi Arabia","Somalia","Sudan","Syria","Tunisia","United Arab Emirates","Yemen"],
-  "BHOC": ['Boris Johnson (Con)', 'Rishi Sunak (Con)', 'Dominic Raab (Con)', 'Priti Patel (Con)', 'Jacob Rees Mogg (Con)', 'David Davis (Con)', 'Keir Starmer (Lab)', 'Lisa Nandy (Lab)', 'Jeremy Corbyn (Lab)', 'Ian Blackford (SNP)', 'Edward Davey (LibDem)', 'Theresa May (Con)', 'Yvette Cooper (Lab)', 'Joy Morrissey (Con)', 'Angela Rayner (Lab)', 'Michael Gove (Con)', 'Colum Easwood (SDLP)', 'Damian Green (Con)', 'John McDonnell (Lab)', 'Ben Wallace (Con)', 'Christopher Chope (Con)', 'Anneliese Dodds (Lab)', 'Robert Buckland (Con)', 'Jeffrey Donaldson (DUP)', 'Johnny Mercer (Con)', 'Harriet Harman (Lab)', 'Kirsty Blackman (SNP)', 'Brandon Lewis (Con)', 'David Lammy (Lab)', 'Imran Ahmad Khan (Con)', 'Alister Jack (Con)', 'Mark Jenkinson (Con)', 'Paula Barker (Lab)', 'Dehenna Davison (Con)']
-}
-let committeeCrisis = $("committee-header").html()
-committeeCountryListsCrisis[committeeCrisis].forEach(function(country){
-  $("#speaker-defaults-crisis").append(`<option value="${country}"></option>`)
-})
 //update the html to reflect changes to the list
 function updateCrisisHTML(list){
   if(list.length < 1){
